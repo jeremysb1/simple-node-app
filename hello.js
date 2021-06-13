4063ff76d12adc6305385b4c0154ec99751af9e6
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-06-13 15:09:15 -0400

    refactor with string interpolation

--- hello.js
+++ hello.js
@@ -3,5 +3,6 @@ const mission = "learn";
 if (mission === "learn") {
   console.log("time to write some Node code");
 } else {
-  console.log("Is " + mission + " really all that fun?");
+  console.log(`Is ${mission} really all that fun?`);
 }
+
